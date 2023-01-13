4bf3ee5c0d3027f960189550789f0b5a8f2ef568
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-01-12 20:49:04 -0600

    gerericos

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,3 @@
     ===== Código de TypeScript =====
 */
 // *-*-*- Genéricos *-*-*-
-function queTipoSoy<T> (argumento: T) {
-    return argumento
-}
-
-let soyString = queTipoSoy('Hola mundo');
-let soyNumber = queTipoSoy(100);
-let soyArreglo = queTipoSoy([1, 2, 3, 4, 5]);
-
-let soyExplicito = queTipoSoy<Number>(100);
